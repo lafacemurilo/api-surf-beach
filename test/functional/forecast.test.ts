503a33d5db963f116ec77d2590eb062117599517
Rafael Perina <rafaelperina.98@gmail.com>
2021-09-10 20:25:13 -0300

    feat: configurado prettier

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,23 +1,21 @@
-import supertest from 'supertest'
-
+import supertest from 'supertest';
 
 /**
  * Descreve um bloco de teste
  */
 describe('Beach forecast funcional test', () => {
+  it('should return a forecast with just a few times', async () => {
+    const { body, status } = await global.testRequest.get('/forecast');
 
-    it('should return a forecast with just a few times', async () => {
+    expect(status).toBe(200);
 
-        const { body, status } = await global.testRequest.get('/forecast');
-
-        expect(status).toBe(200);
-
-        expect(body).toEqual([{
-            forecast: 'cold ❄'
-        },
-        {
-            forecast: 'heat ☀'
-        }]);
-    })
-
-})
+    expect(body).toEqual([
+      {
+        forecast: 'cold ❄',
+      },
+      {
+        forecast: 'heat ☀',
+      },
+    ]);
+  });
+});
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,12 +2,12 @@
  * Arquivo responsavel por inicializar o servidor para todos os testes funcionais
  */
 
-import { SetupServer } from "@src/server";
-import supertest from "supertest";
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
 /** Toda antes de todos os testes da aplicação */
 beforeAll(() => {
-    const server = new SetupServer();
-    server.init();
-    global.testRequest = supertest(server.getApp())
-})
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
